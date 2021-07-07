d5408233d33950d89ff5cc2db181821cca759e13
killzoms <killzoms@DESKTOP-LO74OLE>
2021-07-06 19:16:54 -0700

    Add project files.

---
+++ RebornServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace IslandCreatorPatcher
+{
+    private class Program
+    {
+        private static Thread serverThread;
+
+        private static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                if (Console.ReadLine() == "exit") // basic command line interface
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
